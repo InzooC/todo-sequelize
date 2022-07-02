ddb29cf0d3aeb3ad09a6d759aeeb4473ccef7c8b
InzooC <bluefreaks@gmail.com>
2022-07-02 14:39:35 +0800

    feat: add passport.js and

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
+const usePassport = require('./config/passport')
 const bcrypt = require('bcryptjs')
 const db = require('./models')
 const Todo = db.Todo
@@ -12,31 +15,27 @@ const PORT = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-app.get('/', (req, res) => {
-  return Todo.findAll({
-    raw: true,
-    nest: true
-  })
-    .then((todos) => { return res.render('index', { todos: todos }) })
-    .catch((error) => { return res.status(422).json(error) })
-})
-app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
-    .catch(error => console.log(error))
-})
+usePassport(app)
 
 
 app.get('/users/login', (req, res) => {
   res.render('login')
 })
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
+
 app.get('/users/register', (req, res) => {
   res.render('register')
 })
@@ -69,7 +68,20 @@ app.get('/users/logout', (req, res) => {
   res.send('logout')
 })
 
-
+app.get('/', (req, res) => {
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
+})
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
 
 
 
