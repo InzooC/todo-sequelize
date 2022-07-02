4574e661a9fb8c24aea9d77a50e53c7bc058c517
InzooC <bluefreaks@gmail.com>
2022-07-02 12:02:54 +0800

    feat: add sequelize seeder

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
 
 const app = express()
 const PORT = 3000
@@ -25,7 +28,9 @@ app.get('/users/register', (req, res) => {
   res.render('register')
 })
 app.post('/users/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
 })
 app.get('/users/logout', (req, res) => {
   res.send('logout')
