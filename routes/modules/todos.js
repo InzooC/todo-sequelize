2ee20ec7ba9ea6bed59762a620838f5b8cca0fab
InzooC <bluefreaks@gmail.com>
2022-07-03 14:41:18 +0800

    feat: add edir function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -6,10 +6,31 @@ const db = require('../../models')
 const Todo = db.Todo
 const User = db.User
 
+//查看詳細頁
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+//編輯頁  //編輯頁功能還未完成。
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+router.put('/:id', (req, res) => {
+  // const userId = req.user._id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findByPk(id)
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
